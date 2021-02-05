c9732ff43e1a6c7942b1bbd2678ab4c5335e223c
juanruben <juanruben@gmail.com>
2021-02-05 15:25:12 -0300

    Display json data submitted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import schema from './schema';
 import Form from './Form';
+import Output from './Output';
 import { tw } from 'twind';
 
 const parseProperties = (properties) => {
@@ -9,14 +10,24 @@ const parseProperties = (properties) => {
 
 const App = () => {
     const { type, title, properties } = schema;
+    const [data, setData] = React.useState(null);
 
     if (type === 'object' && properties) {
         const controls = parseProperties(properties);
 
+        const handleAfterSubmit = (data) => {
+            setData(JSON.stringify(data, null, 4));
+        }
+
         return (
             <div className={tw`bg-gray-100 py-20`}>
-                <div className={tw`container mx-auto bg-white flex justify-around shadow-xl rounded-lg p-10`}>
-                    <Form title={title} controls={controls} />
+                <div className={tw`container mx-auto grid grid-cols-2 gap-4`}>
+                    <div className={tw`bg-white shadow-xl rounded-lg p-10`}>
+                        <Form title={title} controls={controls} handleAfterSubmit={handleAfterSubmit}/>
+                    </div>
+                    <div className={tw`bg-white shadow-xl rounded-lg p-10`}>
+                        <Output data={data} />
+                    </div>
                 </div>
             </div>
         );
