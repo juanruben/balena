63c0856c6f7f632de285d9166bbd8b85c7dba564
juanruben <juanruben@gmail.com>
2021-02-04 22:30:07 -0300

    Refactor to extract form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import * as React from 'react';
 import schema from './schema';
-import { useForm } from 'react-hook-form';
-import TagInput from './TagInput';
+import Form from './Form';
 
 const App = () => {
     const { properties } = schema;
-
-    const { register, handleSubmit, setValue, errors } = useForm();
 
     const getProperties = () => {
         let props = [];
@@ -16,94 +13,9 @@ const App = () => {
         return props;
     };
 
-    const getArrayControls = (controls) => {
-        return controls.filter((item) => item.value.type === 'array');
-    };
-
-    const getControl =(item) => {
-        switch(item.value.type){
-            case 'number':
-                return (
-                    <label htmlFor={item.id}>{item.value.title || item.id}
-                        <input name={item.id} id={item.id} type="number" min="0" ref={register({ required: true })} />
-                        {item.value.description}
-                        {errors[item.id] && <span>This field is required</span>}
-                    </label>
-                );
-            case 'string':
-                if (item.value.oneOf?.length) {
-                    return (
-                        <label htmlFor={item.id}>{item.value.title || item.id}
-                            <select name={item.id} id={item.id} ref={register({ required: true })} >
-                                {item.value.oneOf.map((option) => (
-                                    <option key={option.const} value={option.const}>{option.title}</option>
-                                ))}
-                            </select>
-                            {errors[item.id] && <span>This field is required</span>}
-                        </label>
-                    );
-                }
-                return (
-                    <label htmlFor={item.id}>{item.value.title || item.id}
-                        <input
-                            name={item.id}
-                            type={item.value.format === 'date-time' ? "date" : "text"}
-                            minLength={item.value.minLength}
-                            maxLength={item.value.maxLength}
-                            ref={register({ required: true })}
-                        />
-                        {item.value.description}
-                        {errors[item.id] && <span>This field is required</span>}
-                    </label>
-                );
-            case 'array':
-                    if (item.value.items?.type === 'string'){
-                        return (
-                            <label htmlFor={item.id}>{item.value.title || item.id}
-                                <TagInput
-                                    id={item.id}
-                                    name={item.id}
-                                    setValue={setValue}
-                                    inputRef={register}
-                                />
-                                {errors[item.id] && <span>This field is required</span>}
-                            </label>
-                        );
-                    }
-                    return null;
-            default:
-                return null;
-        }
-    };
-
-    const onSubmit = (data) => {
-        console.log(data);
-    }
-
     const controls = getProperties();
 
-    const arrayControls = getArrayControls(controls);
-
-    React.useState(() => {
-        arrayControls.map((item) => {
-            register({ name: item.id }, { required: true });
-            return null;
-        });
-    }, [arrayControls]);
-
-    return (
-        <>
-            <h1>{schema.title}</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                {controls.map((item) => (
-                    <div key={item.id} style={{paddingBottom: '10px'}}>
-                        {getControl(item)}
-                    </div>
-                ))}
-                <input type="submit" />
-            </form>
-        </>
-    );
+    return <Form title={schema.title} controls={controls} />;
 }
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import { useForm } from 'react-hook-form';
+import InputArray from './InputArray';
+
+const Form = ({title, controls}) => {
+
+    const { register, handleSubmit, setValue, errors } = useForm();
+
+    const filterArrayControls = (controls) => {
+        return controls.filter((item) => item.value.type === 'array');
+    };
+
+    const getControl =(item) => {
+        switch(item.value.type){
+            case 'number':
+                return (
+                    <label htmlFor={item.id}>{item.value.title || item.id}
+                        <input name={item.id} id={item.id} type="number" min="0" ref={register({ required: true })} />
+                        {item.value.description}
+                        {errors[item.id] && <span>This field is required</span>}
+                    </label>
+                );
+            case 'string':
+                if (item.value.oneOf?.length) {
+                    return (
+                        <label htmlFor={item.id}>{item.value.title || item.id}
+                            <select name={item.id} id={item.id} ref={register({ required: true })} >
+                                {item.value.oneOf.map((option) => (
+                                    <option key={option.const} value={option.const}>{option.title}</option>
+                                ))}
+                            </select>
+                            {errors[item.id] && <span>This field is required</span>}
+                        </label>
+                    );
+                }
+                return (
+                    <label htmlFor={item.id}>{item.value.title || item.id}
+                        <input
+                            name={item.id}
+                            type={item.value.format === 'date-time' ? "date" : "text"}
+                            minLength={item.value.minLength}
+                            maxLength={item.value.maxLength}
+                            ref={register({ required: true })}
+                        />
+                        {item.value.description}
+                        {errors[item.id] && <span>This field is required</span>}
+                    </label>
+                );
+            case 'array':
+                    if (item.value.items?.type === 'string'){
+                        return (
+                            <InputArray
+                                id={item.id}
+                                label={item.value.title || item.id}
+                                setValue={setValue}
+                                inputRef={register}
+                                errors={errors}
+                            />
+                        );
+                    }
+                    return null;
+            default:
+                return null;
+        }
+    };
+
+    const arrayControls = filterArrayControls(controls);
+
+    React.useState(() => {
+        arrayControls.map((item) => {
+            register({ name: item.id }, { required: true });
+            return null;
+        });
+    }, [arrayControls]);
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+    return (
+        <>
+            <h1>{title}</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {controls.map((item) => (
+                    <div key={item.id} style={{paddingBottom: '10px'}}>
+                        {getControl(item)}
+                    </div>
+                ))}
+                <input type="submit" />
+            </form>
+        </>
+    );
+}
+
+export default Form;
